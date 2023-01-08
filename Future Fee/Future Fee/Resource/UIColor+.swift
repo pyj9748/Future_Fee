32fb9d1996c564a2620bc1b0d8cf62a97c268077
Young June Park <pyj9748@naver.com>
2023-01-08 19:56:13 +0900

    [chore] Color resource UIColor extension으로 구현 (#2)

---
+++ Future Fee/Future Fee/Resource/UIColor+.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+.swift
+//  Future Fee
+//
+//  Created by young june Park on 2023/01/08.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    static let themeGreen = UIColor(red: 93, green: 201, blue: 61, alpha: 1)
+    static let themeRed = UIColor(red: 187, green: 43, blue: 32, alpha: 1)
+}
