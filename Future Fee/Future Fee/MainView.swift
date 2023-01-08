467576b397d708f2b0893fa8a23a560faa7e4c8e
Young June Park <pyj9748@naver.com>
2023-01-08 19:08:09 +0900

    [design] MainView 생성 및 초기화 (#2)

---
+++ Future Fee/Future Fee/MainView.swift
@@ -0,0 +1,34 @@
+//
+//  MainView.swift
+//  Future Fee
+//
+//  Created by young june Park on 2023/01/08.
+//
+
+import UIKit
+
+final class MainView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        addSubview()
+        addConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        
+    }
+    
+    private func addSubview() {
+        
+    }
+
+    private func addConstraint() {
+        
+    }
+}
