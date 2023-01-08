4a0a2a4fb00499b4355a69d461f505db39697b54
Young June Park <pyj9748@naver.com>
2023-01-08 20:14:03 +0900

    [fix] button 코드 개선 (#2)

--- Future Fee/Future Fee/Components/Button.swift
+++ Future Fee/Future Fee/Components/Button.swift
@@ -8,17 +8,33 @@
 import UIKit.UIButton
 
 final class Button: UIButton {
+    private var bgColor: UIColor
+    
     init(backgroundColor: UIColor, text: String) {
+        bgColor = backgroundColor
         super.init(frame: .zero)
-        self.backgroundColor = backgroundColor
-        configure(text)
+        self.backgroundColor = .black
+        configure(backgroundColor: backgroundColor, text: text)
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func configure(_ text: String) {
+    private func configure(backgroundColor: UIColor, text: String) {
         setTitle(text, for: .normal)
+        setTitleColor(.black, for: .normal)
+        layer.borderWidth = 2
+        layer.borderColor = backgroundColor.cgColor
+        clipsToBounds = true
     }
+    
+    func selected() {
+        backgroundColor = bgColor
+    }
+    
+    func deselected() {
+        backgroundColor = .black
+    }
+    
 }
