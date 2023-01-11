119a0d76f0a5c026b826215f0e349f5825b1955f
Young June Park <pyj9748@naver.com>
2023-01-11 19:47:31 +0900

    [refactor] ToolBar 컴포넌트 생성 (#2)

---
+++ Future Fee/Future Fee/Components/ToolBar.swift
@@ -0,0 +1,27 @@
+//
+//  ToolBar.swift
+//  Future Fee
+//
+//  Created by young june Park on 2023/01/11.
+//
+
+import UIKit.UIToolbar
+
+final class ToolBar: UIToolbar {
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        barStyle = UIBarStyle.default
+        isTranslucent = true
+        tintColor = UIColor.white
+        sizeToFit()
+        isUserInteractionEnabled = true
+    }
+}
