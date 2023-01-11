8d6cb03f2dca1d9152f9685c91723024100625f8
Young June Park <pyj9748@naver.com>
2023-01-11 15:51:27 +0900

    [chore] UIColor Extension 생성 (#2)

--- Future Fee/Future Fee/Resource/UIColor+.swift
+++ Future Fee/Future Fee/Resource/UIColor+.swift
@@ -8,6 +8,6 @@
 import UIKit.UIColor
 
 extension UIColor {
-    static let themeGreen = UIColor(red: 93, green: 201, blue: 61, alpha: 1)
-    static let themeRed = UIColor(red: 187, green: 43, blue: 32, alpha: 1)
+    static let themeGreen = UIColor(red: 93 / 255, green: 201 / 255, blue: 61 / 255, alpha: 1)
+    static let themeRed = UIColor(red: 187 / 255, green: 43 / 255, blue: 32 / 255, alpha: 1)
 }
